45e7ea7ed9a2d503ff7d216502bc3ff9cf8301d1
luoxiaochuan <luoxiaochuan@comteck.cn>
2020-05-16 11:46:22 +0800

    feat: 添加 27 题 移除元素

--- 27.移除元素.js
+++ 27.移除元素.js
@@ -62,6 +62,13 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    
+    var i = 0;
+    var len = nums.length;
+    for(var j = 0; j < len; j++){
+        if(nums[j] !== val){
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
 };
-

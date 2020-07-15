3aa9b6cbf1f4ace0937cc5c323469bf6ab452a03
luoxiaochuan <luoxiaochuan@comteck.cn>
2020-07-15 23:29:19 +0800

    feat: 240. 搜索二维矩阵II

---
+++ 240.搜索二维矩阵-ii.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=240 lang=javascript
+ *
+ * [240] 搜索二维矩阵 II
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    return matrix.some(el => el.includes(target));
+};
+// @lc code=end
+

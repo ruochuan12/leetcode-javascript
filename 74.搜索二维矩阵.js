3f6b145bb620ab1d3683ed909283f0e470db9a47
luoxiaochuan <luoxiaochuan@comteck.cn>
2020-07-15 23:07:51 +0800

    feat: 74. 搜索二维矩阵 方法二

--- 74.搜索二维矩阵.js
+++ 74.搜索二维矩阵.js
@@ -10,8 +10,34 @@
  * @param {number} target
  * @return {boolean}
  */
+/**
+ * 方法1
 var searchMatrix = function(matrix, target) {
-    return matrix.some(el => el.includes(target));
+     return matrix.some(el => el.includes(target));
 };
-// @lc code=end
+ */
+/**
+ * 方法2
+ * 
+ */
 
+var searchMatrix = function(matrix, target) {
+    if(!Array.isArray(matrix)) return false;
+    if(matrix.length == 0) return false;
+    if(matrix[0].length == 0) return false;
+    var row = 0, col = matrix[0].length - 1;
+    while(row < matrix.length && col >= 0){
+        if(matrix[row][col] < target){
+            row++;
+        }
+        else if(matrix[row][col] > target){
+            col--;
+        }
+        else{
+            return true;
+        }
+    }
+    return false;
+};
+
+// @lc code=end
